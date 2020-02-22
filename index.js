d21d2c9f5cd51eaf7eefca930f0d667c47cec154
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-02-22 17:58:13 +0100

    Add VillageState class

--- index.js
+++ index.js
@@ -25,3 +25,22 @@ function buildGraph(edges) {
 }
 
 const roadGraph = buildGraph(roads);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
